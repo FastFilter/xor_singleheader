bf4121a6968fce37f41cf4a404f9c1592bdf4c28
Jim Apple <github.public@jbapple.com>
2019-12-23 19:20:40 -0800

    Add fuse filters, which can accommodate higher load
    For large enough sets of keys, Dietzfelbinger & Walzer's fuse filters,
    described in "Dense Peelable Random Uniform Hypergraphs",
    https://arxiv.org/abs/1907.04749, can accomodate fill factors up to
    87.9% full, rather than 1 / 1.23 = 81.3%.
    
    This patch adds an 8-bit version of fuse filters. It does not yet add
    16-bit or buffered filters, which are future (rote) work.

--- benchmarks/bench.c
+++ benchmarks/bench.c
@@ -1,3 +1,4 @@
+#include "fusefilter.h"
 #include "xorfilter.h"
 #include <assert.h>
 #include <time.h>
@@ -111,9 +112,38 @@ bool testbufferedxor16(size_t size) {
   return true;
 }
 
+bool testfuse8(size_t size) {
+  printf("testing fuse8 ");
+  printf("size = %zu \n", size);
+
+  fuse8_t filter;
+
+  fuse8_allocate(size, &filter);
+  // we need some set of values
+  uint64_t *big_set = (uint64_t *)malloc(sizeof(uint64_t) * size);
+  for (size_t i = 0; i < size; i++) {
+    big_set[i] = i; // we use contiguous values
+  }
+  // we construct the filter
+  fuse8_populate(big_set, size, &filter); // warm the cache
+  for (size_t times = 0; times < 5; times++) {
+    clock_t t;
+    t = clock();
+    fuse8_populate(big_set, size, &filter);
+    t = clock() - t;
+    double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
+    printf("It took %f seconds to build an index over %zu values. \n",
+           time_taken, size);
+  }
+  fuse8_free(&filter);
+  free(big_set);
+  return true;
+}
+
 int main() {
   for (size_t s = 10000000; s <= 10000000; s *= 10) {
 
+    testfuse8(s);
     testbufferedxor8(s);
     testxor8(s);
     testbufferedxor16(s);
